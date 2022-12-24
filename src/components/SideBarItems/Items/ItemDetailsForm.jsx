c0f84affcff1c03c4d851ce0957043de8a8ed372
Jonathan Laroco <jonathan.laroco@gmail.com>
2022-12-23 16:27:44 -0800

    HELLO

--- src/components/SideBarItems/Items/ItemDetailsForm.jsx
+++ src/components/SideBarItems/Items/ItemDetailsForm.jsx
@@ -4,7 +4,7 @@ import React, {useState, useEffect} from 'react';
 import { Box, Button, TextField } from "@mui/material";
 
 //FORM
-import { useFormik } from 'formik';
+import { useFormik, Field, FormikProvider } from 'formik';
 import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import InputLabel from '@mui/material/InputLabel';
@@ -24,7 +24,7 @@ const ItemDetailsForm = ({item}) => {
     const colors = tokens(theme.palette.mode);
 
     const isNonMobile = useMediaQuery("(min-width:600px)");
-    const [itemType, setItemType] = useState('');
+    const [itemType, setItemType] = useState(item.ItemType);
     const [nameValue, setNameValue] = useState("");
 
     const onSubmit = (values, actions) => {
@@ -33,10 +33,11 @@ const ItemDetailsForm = ({item}) => {
     }
 
     useEffect(() => {
-        setNameValue(item.Name);
+        //setItemType(item.ItemType);
     }, []);
 
     //Formik
+    console.log("Item Type: " + item.ItemType);
     const formik = useFormik({
         initialValues: {
             name: item.Name,
@@ -60,6 +61,7 @@ const ItemDetailsForm = ({item}) => {
     
     
     return (
+        <FormikProvider value={formik}>
         <form onSubmit={formik.handleSubmit} autoComplete="off">
             <Box
                 display="grid"
@@ -169,12 +171,35 @@ const ItemDetailsForm = ({item}) => {
                     sx={{ gridColumn: "span 2" }}
                 />
                 <FormControl fullWidth>
+                        <TextField
+                            InputLabelProps={{ shrink: true }}
+                            margin="dense"
+                            id="itemtype"
+                            label="Item Type"
+                            fullWidth
+                            variant="filled"
+                            value={formik.values.itemtype}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            sx={{ gridColumn: "span 2" }}
+                            select
+                        >
+                            <MenuItem value="Coffee Beans">Coffee Beans</MenuItem>
+                            <MenuItem value="Nitro Cold Coffee">Nitro Cold Coffee</MenuItem>
+                            <MenuItem value="Gift Box">Gift Box</MenuItem>
+                            <MenuItem value="Other">Other</MenuItem>
+                        </TextField>
+                </FormControl>
+
+                {/*
+                <FormControl fullWidth>
                     <InputLabel>Item Type</InputLabel>
                         <Select
                             id="itemtype"
                             value={formik.values.itemtype}
                             label="Item Type"
                             onChange={formik.handleChange}
+                            fullWidth
                             required
                         >
                             <MenuItem value="Coffee Beans">Coffee Beans</MenuItem>
@@ -183,6 +208,8 @@ const ItemDetailsForm = ({item}) => {
                             <MenuItem value="Other">Other</MenuItem>
                         </Select>
                 </FormControl>
+            */}
+
             </Box>
             <Box display="flex" justifyContent="end" mt="20px">
                 <Button 
@@ -198,145 +225,8 @@ const ItemDetailsForm = ({item}) => {
                 </Button>
             </Box>
         </form>
+        </FormikProvider>
     )
-        {/*
-        <Formik
-            onSubmit={handleFormSubmit}
-            initialValues={initialValues}
-            validationSchema={checkoutSchema}
-            >
-            {({
-                values,
-                errors,
-                touched,
-                handleBlur,
-                handleChange,
-                handleSubmit,
-            }) => (
-                <form onSubmit={handleSubmit}>
-                <Box
-                    display="grid"
-                    gap="30px"
-                    gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-                    sx={{
-                    "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-                    }}
-                >
-                    <TextField
-                        fullWidth
-                        variant="filled"
-                        type="text"
-                        label="Name"
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values.name}
-                        name="name"
-                        id="name"
-                        error={!!touched.name && !!errors.name}
-                        helperText={touched.name && errors.name}
-                        sx={{ gridColumn: "span 2" }}
-                    />
-
-                    <TextField
-                        fullWidth
-                        variant="filled"
-                        type="text"
-                        label="Description"
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values.description}
-                        name="description"
-                        id="description"
-                        error={!!touched.description && !!errors.description}
-                        helperText={touched.description && errors.description}
-                        sx={{ gridColumn: "span 2" }}
-                    />
-
-                    <TextField
-                        fullWidth
-                        variant="filled"
-                        type="text"
-                        label="Region"
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values.region}
-                        name="region"
-                        id="region"
-                        error={!!touched.region && !!errors.region}
-                        helperText={touched.region && errors.region}
-                        sx={{ gridColumn: "span 2" }}
-                    />
-
-                    <TextField
-                        fullWidth
-                        variant="filled"
-                        type="text"
-                        label="Elevation"
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values.elevation}
-                        name="elevation"
-                        id="elevation"
-                        error={!!touched.elevation && !!errors.elevation}
-                        helperText={touched.elevation && errors.elevation}
-                        sx={{ gridColumn: "span 2" }}
-                    />
-
-                    <TextField
-                        fullWidth
-                        variant="filled"
-                        type="number"
-                        label="Item ID"
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values.itemId}
-                        name="itemId"
-                        id="itemId"
-                        error={!!touched.itemId && !!errors.itemId}
-                        helperText={touched.itemId && errors.itemId}
-                        sx={{ gridColumn: "span 2" }}
-                    />
-
-                    <TextField
-                        fullWidth
-                        variant="filled"
-                        type="number"
-                        label="Giftbox Items"
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values.giftBoxItems}
-                        name="giftBoxItems"
-                        id="giftBoxItems"
-                        error={!!touched.giftBoxItems && !!errors.giftBoxItems}
-                        helperText={touched.giftBoxItems && errors.giftBoxItems}
-                        sx={{ gridColumn: "span 2" }}
-                    />
-
-                    <FormControl fullWidth>
-                        <InputLabel>Item Type</InputLabel>
-                            <Select
-                                id="itemType"
-                                value={itemType}
-                                label="Item Type"
-                                onChange={handleItemTypeChange}
-                            >
-                                <MenuItem value="Coffee Beans">Coffee Beans</MenuItem>
-                                <MenuItem value="Nitro Cold Coffee">Nitro Cold Coffee</MenuItem>
-                                <MenuItem value="Gift Box">Gift Box</MenuItem>
-                                <MenuItem value="Other">Other</MenuItem>
-                            </Select>
-                    </FormControl>
-                </Box>
-                <Box display="flex" justifyContent="end" mt="20px">
-                    <Button type="submit" color="secondary" variant="contained">
-                        Save
-                    </Button>
-                </Box>
-                </form>
-            )}
-            </Formik>
-        */}
-    
 }
 
 export default ItemDetailsForm
