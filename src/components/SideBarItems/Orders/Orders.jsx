52011959ca7327af143e775d3a7db1b900fe2e97
Jonathan Laroco <jonathan.laroco@gmail.com>
2023-01-03 21:09:47 -0800

    Adding ORders

--- src/components/SideBarItems/Orders/Orders.jsx
+++ src/components/SideBarItems/Orders/Orders.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
+
+//Light/Dark Mode
+import { ColorModeContext, useMode } from '../../../theme';
+import { Box, CssBaseline, ThemeProvider } from '@mui/material';
+
+//Components
+import Topbar from "../../NAVBars/TopBar";
+import Sidebar from "../../NAVBars/SideBar";
+import Header from "../../Header/Header";
+import OrdersList from './OrdersList';
 
 const Orders = () => {
+  const [theme, colorMode] = useMode();
+
+
   return (
-    <div>Orders</div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+          <div className="app">
+            <Sidebar />
+            <main className="content">
+              <Topbar />
+                <Box m="20px">
+                    {/* HEADER */}
+                    <Box display="flex" justifyContent="space-between" alignItems="center">
+                      <Header title="ORDERS" subtitle='' />
+                    </Box>
+                </Box>
+                <div className='w-100 text-center mt-2'>
+                  <OrdersList />
+                </div>
+            </main>
+          </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 
---
+++ src/components/SideBarItems/Orders/OrdersList.jsx
@@ -0,0 +1,109 @@
+import React, {useState, useEffect} from 'react';
+import { Box, useTheme, } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../../theme";
+import { db } from '../../../Firebase/firebase';
+import { collection, query, onSnapshot, orderBy} from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
+
+const OrdersList = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+    const [orders, setOrders] = useState([]);
+    const navigate = useNavigate();
+
+    const fetchOrderData = async ()=>{
+        const couponsCollection = query(collection(db,'orders'),orderBy("OrderDate", "desc"));
+        onSnapshot(couponsCollection, (querySnapshot) => {
+            const ordersList = [];
+            querySnapshot.forEach((doc) => {
+                var orderData = {
+                    id: doc.id,
+                    FullName: doc.data().FullName,
+                    OrderType: doc.data().OrderType,
+                    Total: doc.data().Total
+                }
+                ordersList.push(orderData);
+            });
+            setOrders(ordersList);
+        });
+    }
+
+    const columns = [
+        {
+            field: "FullName", 
+            headerName: "Name", 
+            flex: 1,
+        },
+        {
+            field: "OrderType", 
+            headerName: "Order Type", 
+            flex: 1
+        }
+        ,
+        {
+            field: "Total", 
+            headerName: "Total ($)", 
+            flex: 1
+        }
+    ]
+
+    useEffect(() => {
+        fetchOrderData();
+      }, []);
+    
+    
+      const onRowsSelectionHandler = (ids) => {
+          //const selectedRowsData = ids.map((id) => items.find((row) => row.id === id));
+          //itemsToRemove({selectedRowsData});
+      };
+    
+      const handleRowClick = (params) => {
+        //navigate("/itemdetails/" + params.row.id);
+      };
+
+    return (
+        <Box m="20px">
+            <Box
+                m="20px 0 0 0"
+                height="50vh"
+                sx={{
+                "& .MuiDataGrid-root": {
+                    border: "none",
+                },
+                "& .MuiDataGrid-cell": {
+                    borderBottom: "none",
+                },
+                "& .name-column--cell": {
+                    color: colors.greenAccent[300],
+                },
+                "& .MuiDataGrid-columnHeaders": {
+                    backgroundColor: colors.blueAccent[700],
+                    borderBottom: "none",
+                },
+                "& .MuiDataGrid-virtualScroller": {
+                    backgroundColor: colors.primary[400],
+                },
+                "& .MuiDataGrid-footerContainer": {
+                    borderTop: "none",
+                    backgroundColor: colors.blueAccent[700],
+                },
+                "& .MuiCheckbox-root": {
+                    color: `${colors.greenAccent[200]} !important`,
+                },
+                }}
+            >
+                <DataGrid 
+                    rows={orders} 
+                    columns={columns} 
+                    pageSize={50}
+                    onSelectionModelChange={(ids) => onRowsSelectionHandler(ids)}
+                    onRowDoubleClick={handleRowClick}
+                />
+            </Box>
+        </Box>
+    )
+    }
+
+export default OrdersList
